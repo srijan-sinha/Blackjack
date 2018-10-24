3a69178dcb3559bf72da0f3acc30438c4281f20a
akshat-khare <akshat.khare08@gmail.com>
2018-10-25 04:54:46 +0530

    addition also added to main

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -5,4 +5,12 @@ int main(int argc, char** argv)
     mgame->fillTable();
     mgame->updateTable();
     mgame->printTable();
+    for(int i=0;i<38;i++){
+        double ct=0;
+        for(int j=0;j<38;j++){
+            ct += mgame->transProb[i][j][0];
+        }
+        cout<<ct<<endl;
+    }
+    cout << "done"<<endl;
 }
